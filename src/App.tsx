6117ed1b511dcaa0cfe855d637312f4dee5af667
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2021-10-14 09:17:51 +0300

    Minor update to static text

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-	<h2>Additional header in between</h2>
+        <h2>Additional header in between</h2>
+        <h5>Once more changes..</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
